99cfea4cecc8307a1003b08d15e95a8e56989e2b
Natheem-ScottST10109685 <ST10109685@vcconnect.edu.za>
2024-08-29 13:38:03 +0100

    Second Commit
    App service working now

--- NatheemScott_ST10109685_CLDVPOEPart1/Models/CustomerProf.cs
+++ NatheemScott_ST10109685_CLDVPOEPart1/Models/CustomerProf.cs
@@ -10,10 +10,10 @@ namespace NatheemScott_ST10109685_CLDVPOEPart1.Models
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
         // Custom properties
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
 
         public CustomerProf()
         {
